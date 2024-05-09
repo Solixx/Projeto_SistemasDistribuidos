61f0ce5ddcda352f72c584e148cb2ac8325ee018
Manuel <manuel_jose_2002@hotmail.com>
2024-05-09 15:03:22 +0100

    Clear proj

--- src/GameFactoryRI.java
+++
@@ -1,5 +0,0 @@
-public interface GameFactoryRI {
-    public void listGames() throws Exception;
-    public boolean register(String username, String pwd) throws Exception;
-    public DigLibSessionRI login(String username, String pwd) throws Exception;
-}
